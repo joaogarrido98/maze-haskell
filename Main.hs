ceb3433b26786eff2064146b373b496de7b0d140
João Garrido <48774418+joaogarrido98@users.noreply.github.com>
2021-01-03 21:39:11 +0000

    Add files via upload

--- Main.hs
+++ Main.hs
@@ -2,11 +2,10 @@ module Main (get_maze, print_maze, is_wall, place_player, move, can_move, game_l
 
 import System.Environment
 
-maze_path = "C:\\Users\\joaom\\Desktop\\uni\\Programming Paradigms\\semester1\\assignment3\\a3windows\\maze-big-4.txt"
+maze_path = "C:\\Users\\joaom\\Desktop\\uni\\Programming Paradigms\\semester1\\assignment3\\a3windows\\maze4.txt"
 
 -- Useful code from Lecture 25
 -- You may use this freely in your solutions
-
 get :: [String] -> Int -> Int -> Char
 get maze x y = (maze !! y) !! x
 
@@ -24,10 +23,9 @@ set maze x y char =
    in new_maze
 
 ---- Part A
-
 -- Question 1
 --if path is empty return empty list
--- else read file, unbox it and then return the unboxed with lines function
+--else read file, unbox it and then return the unboxed with lines function
 get_maze :: String -> IO [String]
 get_maze [] = return []
 get_maze path = do
@@ -45,18 +43,19 @@ print_maze maze = do
   putStrLn final
 
 -- Question 3
--- if maze is empty return false
+-- if maze is empty return an error because  is_wall [] (x,y) = False doesn't make sense here 
 -- if get maze coordinates returns empty then true else false
 is_wall :: [String] -> (Int, Int) -> Bool
-is_wall [] (x, y) = False
+is_wall [] (x, y) = error "Empty Maze"
 is_wall maze (x, y)
   | returned == ' ' = False
   | returned == '#' = True
-  where
-    returned = get maze x y
+  where returned = get maze x y
+        (z,v) = get_target_position maze
 
 -- Question 4
 -- if maze is empty return an empty board
+-- else unbox the set function with the new char in the (x,y) position
 place_player :: [String] -> (Int, Int) -> [String]
 place_player [] (x, y) = []
 place_player maze (x, y) = do
@@ -64,7 +63,6 @@ place_player maze (x, y) = do
   return setting
 
 ---- Part B
-
 -- Question 5
 --if dir is anything but wasd returns the same place
 -- else adds or subtracts from the location is on
@@ -77,16 +75,16 @@ move (x, y) dir
   | otherwise = (x, y)
 
 -- Question 6
--- check base cases
 -- get the place where user wants to move from move function then
 -- with that value check if is wall if its wall return false else true
 can_move :: [String] -> (Int, Int) -> Char -> Bool
-can_move [] (x, y) dir = False
+can_move [] (x, y) dir = error "Empty maze"
 can_move maze (x, y) dir = if is_wall maze movement == True then False else True
   where
     movement = move (x, y) dir
 
 --Question 7
+--put everything together to be able to play the game
 game_loop :: [String] -> (Int, Int) -> IO ()
 game_loop [] (x, y) = putStrLn []
 game_loop maze (x, y) = do
@@ -99,9 +97,11 @@ game_loop maze (x, y) = do
 
 ---- Part C
 -- Question 8
+-- call an auxiliary function
 get_path :: [String] -> (Int, Int) -> (Int, Int) -> [(Int, Int)]
 get_path maze start end = head (get_path_aux maze [start] end [])
 
+--if 
 get_path_aux :: [String] -> [(Int, Int)] -> (Int,Int) -> [(Int,Int)]-> [[(Int,Int)]]
 get_path_aux maze list end visited
   | list == [] = []
@@ -110,6 +110,8 @@ get_path_aux maze list end visited
   where x = head list
         xs = tail list
 
+--from w a s d filter the ways it can move
+-- then maps all of returned values from the filter with move to get the actual tuple of the positions
 get_possible_move :: [String] -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
 get_possible_move maze start visited = filter (\x -> not (x `elem` visited)) maped
                                      where filtro = filter (\dir -> is_wall maze (move start dir) == False) moves
@@ -118,6 +120,9 @@ get_possible_move maze start visited = filter (\x -> not (x `elem` visited)) map
 
 -- Question 9
 ---- Part B
+--get arguments inputed in the cmd then get the head, read the file use the line function
+--after that call the get_path function to get the possible path of the maze
+-- then print the maze with the dots using get_dotted_maze function
 main :: IO ()
 main = do
        args <- getArgs
@@ -136,15 +141,18 @@ place_dot :: [String] -> (Int, Int) -> [String]
 place_dot [] (x, y) = []
 place_dot maze (x, y) = set maze x y '.'
 
+--function that puts dots in each position required on the path
 placing_dots_together :: [String] -> [(Int, Int)] -> [[String]]
 placing_dots_together maze [] = []
 placing_dots_together maze (x : xs) = place_dot maze_up x : placing_dots_together maze_up xs
                           where maze_up = place_dot maze x
 
+--print the maze with the value returned from placing_dots_together
+--after reversing it and getting the head of it
 get_dotted_maze :: [String] -> [(Int, Int)] -> IO ()
 get_dotted_maze maze visited = print_maze(head (reverse (placing_dots_together maze visited)))
 
-
+--get the length of the maze to find the target position
 get_target_position :: Foldable t => [t a] -> (Int, Int)
 get_target_position (x : xs) = (col - 2, row - 2)
                                where col = length x
